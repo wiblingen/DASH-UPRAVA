24f8efda3517a52f769e450f2f6516b18d94f17c
Chipster <go@w.ay>
2023-06-08 10:20:19 -0500

    * Minor: use "WPSD" in footers (part of rebrand)

--- admin/advanced/jitter_test.php
+++ admin/advanced/jitter_test.php
@@ -154,7 +154,7 @@ Test Complete.
   </div>
   <div class="footer">
   Pi-Star web config, &copy; Andy Taylor (MW0MWZ) 2014-<?php echo date("Y"); ?>.<br />
-  <a href="https://w0chp.net/w0chp-pistar-dash/" style="color: #ffffff; text-decoration:underline;">W0CHP-PiStar-Dash</a> by W0CHP
+  <a href="https://w0chp.net/w0chp-pistar-dash/" style="color: #ffffff; text-decoration:underline;">W0CHP-PiStar-Dash (WPSD)</a> by W0CHP
   <br />
   </div>
   </div>
@@ -182,7 +182,7 @@ Test Complete.
   </div>
   <div class="footer">
   Pi-Star web config, &copy; Andy Taylor (MW0MWZ) 2014-<?php echo date("Y"); ?>.<br />
-  <a href="https://w0chp.net/w0chp-pistar-dash/" style="color: #ffffff; text-decoration:underline;">W0CHP-PiStar-Dash</a> by W0CHP
+  <a href="https://w0chp.net/w0chp-pistar-dash/" style="color: #ffffff; text-decoration:underline;">W0CHP-PiStar-Dash (WPSD)</a> by W0CHP
   <br />
   </div>
   </div>
