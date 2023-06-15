9a9218591eda64f989053994d30a819f0e41ae58
Chipster <go@w.ay>
2023-06-15 06:55:22 -0500

    * Minor: fix typo

--- includes/execute-background-tasks.php
+++ includes/execute-background-tasks.php
@@ -1,5 +1,5 @@
 <?php
-exec('sudo /usr/local/sbin/background-tasks.sh &> /dev/null 2<&1');
+exec('sudo /usr/local/sbin/background-tasks.sh > /dev/null 2>&1 &');
 touch('/tmp/.last-index-bg-exec');
 ?>
 
