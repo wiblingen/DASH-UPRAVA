4d667e92ebf90ced4d6149e706f5bca5fb56ef54
Chipster <go@w.ay>
2023-11-06 09:42:45 -0600

    * Minor: add HW UUID to admin/system details page for diags/suport; tweak Bi7JTA Nano_hotSPOT modem string in radio info pane

--- mmdvmhost/radioinfo.php
+++ mmdvmhost/radioinfo.php
@@ -1,8 +1,11 @@
 <?php
 
+
 if (!isset($_SESSION) || !is_array($_SESSION)) {
     session_id('pistardashsess');
     session_start();
+
+    //unset($_SESSION['DvModemFWVersion']);  // unset the modem FW version in the event the user up/downgraded, so it shows the correct ver.
     
     include_once $_SERVER['DOCUMENT_ROOT'].'/config/config.php';          // MMDVMDash Config
     include_once $_SERVER['DOCUMENT_ROOT'].'/mmdvmhost/tools.php';        // MMDVMDash Tools
