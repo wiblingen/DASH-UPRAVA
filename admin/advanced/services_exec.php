6e6bad63bc430075850ea99217b5b911db4ec911
Chipster <go@w.ay>
2023-11-08 03:08:53 -0600

    * Minor: code updates to reflect updated backend chages

--- admin/advanced/services_exec.php
+++ admin/advanced/services_exec.php
@@ -16,22 +16,22 @@ $action = isset($_GET['action']) ? $_GET['action'] : '';
 $cmdoutput = array();
 switch ($action) {
     case "stop":
-	$cmdresult = exec('sudo /usr/local/sbin/pistar-services stop', $cmdoutput, $retvalue);
+	$cmdresult = exec('sudo /usr/local/sbin/wpsd-services stop', $cmdoutput, $retvalue);
 	break;
     case "fullstop":
-	$cmdresult = exec('sudo /usr/local/sbin/pistar-services fullstop', $cmdoutput, $retvalue);
+	$cmdresult = exec('sudo /usr/local/sbin/wpsd-services fullstop', $cmdoutput, $retvalue);
 	break;
     case "restart":
-	$cmdresult = exec('sudo /usr/local/sbin/pistar-services restart', $cmdoutput, $retvalue);
+	$cmdresult = exec('sudo /usr/local/sbin/wpsd-services restart', $cmdoutput, $retvalue);
 	break;
     case "status":
-	$cmdresult = exec('sudo /usr/local/sbin/pistar-services status', $cmdoutput, $retvalue);
+	$cmdresult = exec('sudo /usr/local/sbin/wpsd-services status', $cmdoutput, $retvalue);
 	break;
     case "killmmdvmhost":
 	$cmdresult = exec('sudo /usr/bin/killall -q -9 MMDVMHost', $cmdoutput, $retvalue);
 	break;
     case "updatehostsfiles":
-	$cmdresult = exec('sudo -- /bin/bash -c "env FORCE=1 /usr/local/sbin/HostFilesUpdate.sh; /usr/local/sbin/pistar-services restart;"', $cmdoutput, $retvalue);
+	$cmdresult = exec('sudo -- /bin/bash -c "env FORCE=1 /usr/local/sbin/HostFilesUpdate.sh; /usr/local/sbin/wpsd-services restart;"', $cmdoutput, $retvalue);
 	break;
     default:
 	$cmdoutput = array('error !');
