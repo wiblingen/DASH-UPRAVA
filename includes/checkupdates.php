cda12c614f3f14b68baf7e05eb7a5106bf2bc587
Chipster <go@w.ay>
2023-12-04 18:02:12 -0600

    * Minor: cleanups

--- includes/checkupdates.php
+++ includes/checkupdates.php
@@ -3,7 +3,7 @@ require_once $_SERVER['DOCUMENT_ROOT'].'/config/config.php';
 require_once $_SERVER['DOCUMENT_ROOT'].'/config/version.php';
 
 if (constant("AUTO_UPDATE_CHECK") == "true") {
-   echo $version; system('/usr/local/sbin/pistar-check4updates');
+   echo $version; system('/usr/local/sbin/.wpsd-check4updates');
 } else {
     echo $version;
 }
