f7486d7e90b806ba14713646ca0e8008f183ab5e
Chipster <go@w.ay>
2023-12-18 21:50:42 -0600

    * Minor: code cleanups

--- admin/profile_manager.php
+++ admin/profile_manager.php
@@ -43,7 +43,7 @@ if (file_exists('/etc/.WPSD_config') && count(glob("$config_dir/*")) > 0) {
 ?>
     <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
 	      "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-    <html xmlns="http://www.w3.org/1999/xhtml" xmlns:v="urn:schemas-microsoft-com:vml" lang="en">
+    <html lang="en">
 	<head>
 	    <meta name="language" content="English" />
 	    <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
