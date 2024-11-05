bc0f9a744b0c8319fb3be41522bf083da9b7f8b3
Chipster <go@w.ay>
2024-11-05 09:09:26 -0600

    session mgmt

--- includes/execute-background-tasks.php
+++ includes/execute-background-tasks.php
@@ -1,5 +1,13 @@
 <?php
 
+session_set_cookie_params(0, "/");
+session_name("WPSD_Session");
+session_id('wpsdsession');
+session_start();
+
+require_once $_SERVER['DOCUMENT_ROOT'].'/config/config.php';
+require_once $_SERVER['DOCUMENT_ROOT'].'/config/version.php';
+
 $UUID = $_SESSION['WPSDrelease']['WPSD']['UUID'];
 $CALL = $_SESSION['WPSDrelease']['WPSD']['Callsign'];
 $UA = "$CALL $UUID";
