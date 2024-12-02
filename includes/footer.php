5df685031826a9ec1ad8399b5e9904a4a4f313a6
Chipster <go@w.ay>
2024-12-02 17:43:16 -0600

    cleanup footer

--- includes/footer.php
+++ includes/footer.php
@@ -1,6 +1,6 @@
         <div class="footer">
            <?php
-                echo 'Get WPSD Help: [ <a href="https://w0chp.radio/wpsd-faqs/" target="_new">FAQs</a> ] &bull; [ <a href="https://manual.wpsd.radio/" target="_new">User Manual</a> ] &bull; [ <a href="https://www.facebook.com/groups/wpsdproject/" target="_new">Facebook Group</a> ] &bull; [ <a href="https://discord.gg/b8Hv5ygPdF" target="_new">Discord Server</a> ]<br />';
+                echo 'Get WPSD Help: <a href="https://w0chp.radio/wpsd-faqs/" target="_new">FAQs</a> &bull; <a href="https://manual.wpsd.radio/" target="_new">User Manual</a> &bull; <a href="https://www.facebook.com/groups/wpsdproject/" target="_new">Facebook Group</a> &bull; <a href="https://discord.gg/b8Hv5ygPdF" target="_new">Discord Server</a><br />';
                 echo '<a href="https://wpsd.radio/">WPSD</a> by Chip Cuccio, <code>W0CHP</code> &copy; 2020-'.date("Y").' -- WPSD Project <a href="https://w0chp.radio/wpsd/#credits" target="_new">Credits</a>';
            ?>
         </div>
